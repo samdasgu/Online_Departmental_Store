8b6314e86bbdcfc218c56f8dbaeb6310057c75dc
samdasgupta <samdasgupta@USHYDSAMDASGU3.us.deloitte.com>
2020-09-21 21:19:12 +0530

    Created Discovery Server

---
+++ discovery-server/src/main/java/org/samdasgu/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,20 @@
+package org.samdasgu.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Discovery server
+ * @author samdasgupta
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+
+}
